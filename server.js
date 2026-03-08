3f42caf507feab4b3c70930de0c8520b8c3f77e9
Thắng Đồng <thangdong@gmail.com>
2026-03-08 16:06:37 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+require("module-alias/register");
+require("./polyfill");
+
+const express = require("express");
+const cors = require("cors");
+
+const rootRouter = require("@/routes");
+const customResponse = require("@/middlewares/customResponse");
+const errorHandle = require("@/middlewares/errorHandle");
+const notFoundHandle = require("@/middlewares/notFoundHandle");
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(customResponse);
+app.use(express.static("public"));
+
+// Router
+app.use("/api", rootRouter);
+
+// Error handle
+app.use(notFoundHandle);
+app.use(errorHandle);
+
+app.listen(port, () => {
+  console.log(`Demo app listening on port ${port}`);
+});
